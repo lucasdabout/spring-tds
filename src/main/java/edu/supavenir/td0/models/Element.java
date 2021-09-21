e77e4a39d33990e48a9841bb0381ca0b9dc2f1ad
J-C HERON <myaddressmail@gmail.com>
2021-09-21 11:57:26 +0200

    add ItemsController

---
+++ src/main/java/edu/supavenir/td0/models/Element.java
@@ -0,0 +1,22 @@
+package edu.supavenir.td0.models;
+
+public class Element {
+	private String nom;
+	private int evaluation;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getEvaluation() {
+		return evaluation;
+	}
+
+	public void setEvaluation(int evaluation) {
+		this.evaluation = evaluation;
+	}
+}
